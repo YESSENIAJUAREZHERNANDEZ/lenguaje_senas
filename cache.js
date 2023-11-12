d7e67f1f0f7a6536ae7566d433303161d4af8814
mediplan <1720110342@utectulancingo.edu.mx>
2023-11-12 14:12:54 -0600

    iconos

--- cache.js
+++ cache.js
@@ -1,8 +1,9 @@
 const browserInformation = {
-    name: 'Opera',
-    version: 108
-  };
-  
-  localStorage.setItem('browser', JSON.stringify(browserInformation));
+  name: 'Opera',
+  version: 108
+};
+
+localStorage.setItem('browser', JSON.stringify(browserInformation));
 
 const value = localStorage.getItem('browser');
+console.log(JSON.parse(value));
